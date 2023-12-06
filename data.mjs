490cf3d749271bcfbc04021511e3b87d6b3e0207
Shant Dashjian <shantgdashjian@gmail.com>
2023-12-06 18:00:04 +0400

    Perform the embeddings and storage to supabase vector database

--- data.mjs
+++ data.mjs
@@ -1,12 +1,27 @@
 import { PDFLoader } from "langchain/document_loaders/fs/pdf"
-
-const loader = new PDFLoader("book.pdf", {
-  splitPages: true,
-})
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseVectorStore } from 'langchain/vectorstores/supabase'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 
 try {
+  const loader = new PDFLoader("book.pdf", {
+    splitPages: true,
+  })
   const docs = await loader.load()
-  console.log(docs)
+  
+  const supabaseApiKey = process.env.SUPABASE_API_KEY
+  const supabaseProjectUrl = process.env.SUPABASE_PROJECT_URL
+  const openAIApiKey = process.env.OPENAI_API_KEY
+
+  const client = createClient(supabaseProjectUrl, supabaseApiKey)
+  await SupabaseVectorStore.fromDocuments(
+    docs,
+    new OpenAIEmbeddings({ openAIApiKey }),
+    {
+      client,
+      tableName: 'cloud_native_spring_documents',
+    }
+  )
 } catch (err) {
   console.log(err)
 }
