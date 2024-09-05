a8ef495d5d1dd2bd92583b2d66fcdd32cc68b14e
Shant Dashjian <shantgdashjian@gmail.com>
2024-09-05 19:20:42 +0400

    Extract AI and Supabase code out to the worker

--- index.js
+++ index.js
@@ -1,64 +1,11 @@
-import { ChatOpenAI } from "langchain/chat_models/openai"
-import { PromptTemplate } from "langchain/prompts"
-import { StringOutputParser } from 'langchain/schema/output_parser'
-import { retriever } from "./utils/retreiver"
-import { combineDocuments } from "./utils/combineDocuments"
-import { RunnablePassthrough, RunnableSequence } from "langchain/schema/runnable"
-import formatConversationHistory from "./utils/formatConversationHistory"
+const workerUrl = 'https://cloud-native-spring-expert-worker.shant.workers.dev/'
 
 document.addEventListener('submit', (e) => {
   e.preventDefault()
   progressConversation()
 })
 
-const openAIApiKey = import.meta.env.VITE_OPENAI_API_KEY
-
-const llm = new ChatOpenAI({ openAIApiKey })
-
-const standaloneQuestionTemplate = `Given a question and the converstaion history, convert the question to a standalone question. You can use the converstaion history as a resource as well. 
-question: {question} 
-conversation history: {conversation_history}
-standalone question: `
-
-const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
-
-const answerTemplate = `You are a friendly and enthusiastic expert who can answer a given question about Cloud Native Spring based on the context provided. Try to find the answer in the context as it is the primary source of knowledge. You could also use the conversation history if you cannot find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." Don't try to make up an answer. Be friendly and make the response conversational and relatively short.
-context: {context}
-conversation history: {conversation_history}
-question: {question}
-answer: `
-
-const answerPrompt = PromptTemplate.fromTemplate(answerTemplate)
-
-const standaloneQuestionChain = standaloneQuestionPrompt
-  .pipe(llm)
-  .pipe(new StringOutputParser())
-
-const retrieverChain = RunnableSequence.from([
-  prevResult => prevResult.standalone_question,
-  retriever,
-  combineDocuments
-])
-
-const answerChain = answerPrompt
-  .pipe(llm)
-  .pipe(new StringOutputParser())
-
 const conversationHistory = []
-
-const chain = RunnableSequence.from([
-  {
-    standalone_question: standaloneQuestionChain,
-    original_input: new RunnablePassthrough()
-  },
-  {
-    context: retrieverChain,
-    question: ({ original_input }) => original_input.question,
-    conversation_history: ({ original_input }) => original_input.conversation_history
-  },
-  answerChain
-])
-
 
 async function progressConversation() {
   const userInput = document.getElementById('user-input')
@@ -72,19 +19,33 @@ async function progressConversation() {
   personalAssistantConversation.appendChild(newHumanSpeechBubble)
   newHumanSpeechBubble.textContent = question
   personalAssistantConversation.scrollTop = personalAssistantConversation.scrollHeight
-  const response = await chain.invoke({
-    question: question,
-    conversation_history: formatConversationHistory(conversationHistory)
-  })
 
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ question: question, conversationHistory: conversationHistory })
+  }
+  let answer = ''
+  try {
+    const response = await fetch(workerUrl, options)
+    
+    answer = await response.json()
+    if (!response.ok) {
+      throw new Error(answer.error)
+    }
+  } catch(error) {
+    answer = error
+  }
   // Add to memory
   conversationHistory.push(question)
-  conversationHistory.push(response)
+  conversationHistory.push(answer)
 
   // Add AI message
   const newAiSpeechBubble = document.createElement('div')
   newAiSpeechBubble.classList.add('speech', 'speech-ai')
   personalAssistantConversation.appendChild(newAiSpeechBubble)
-  newAiSpeechBubble.textContent = response
+  newAiSpeechBubble.textContent = answer
   personalAssistantConversation.scrollTop = personalAssistantConversation.scrollHeight
 }
--- utils/formatConversationHistory.js
+++
@@ -1,9 +0,0 @@
-export default function formatConversationHistory(messages) {
-  return messages.map((message, i) => {
-      if (i % 2 === 0){
-          return `Human: ${message}`
-      } else {
-          return `Expert: ${message}`
-      }
-  }).join('\n')
-}
